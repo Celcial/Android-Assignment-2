3e523245b27fa7f8509bdf899440f3c626d218e9
David <DavidGray@Outlook.co.nz>
2017-05-26 14:56:32 +1200

    All layouts added

---
+++ app/src/main/java/com/example/david_2/petshop/PetSchedule.java
@@ -0,0 +1,13 @@
+package com.example.david_2.petshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PetSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_schedule);
+    }
+}
