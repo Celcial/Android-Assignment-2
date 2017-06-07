2f647776539b77bc68f00a9d9b9ec13afb87413d
David <DavidGray@Outlook.co.nz>
2017-06-07 18:39:42 +1200

    petInfo changes

--- app/src/main/java/com/example/david_2/petshop/PetInfo.java
+++ app/src/main/java/com/example/david_2/petshop/PetInfo.java
@@ -1,13 +1,25 @@
 package com.example.david_2.petshop;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PetInfo extends AppCompatActivity {
+    TextView petsName;
+    Typeface custom_font;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_info);
+
+        custom_font = Typeface.createFromAsset(getAssets(),"fonts/moonlight.ttf");
+
+
+        petsName = (TextView)findViewById(R.id.tv_pet_name);
+        petsName.setTypeface(custom_font);
+
+
     }
 }
